dcbae9345ba5424d0aac9ddea5df6c645662fddc
Martin Larsson <larssonmartin1998@gmail.com>
2024-04-14 12:53:39 +0200

    Add window configuration

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ pub mod movement;
 pub mod velocity;
 
 use bevy::prelude::*;
+use bevy::window::{EnabledButtons, WindowMode, WindowPosition, WindowResolution};
 
 fn main() {
     App::new()
@@ -21,5 +22,30 @@ fn main() {
             DefaultPlugins.set(ImagePlugin::default_nearest()),
             dark_arts_defense::DarkArtsDefensePlugin,
         ))
+        .add_systems(Startup, setup_window)
         .run();
+}
+
+fn setup_window(mut query: Query<&mut Window>) {
+    let mut window = query.single_mut();
+    window.cursor.visible = false;
+    window.mode = WindowMode::BorderlessFullscreen;
+    window.resolution = WindowResolution::new(1920.0, 1080.0);
+    window.title = "Dark Arts Defense".to_owned();
+    window.resize_constraints = WindowResizeConstraints {
+        min_width: 1280.0,
+        min_height: 720.0,
+        max_width: 3840.0,
+        max_height: 2160.0,
+    };
+    window.resizable = true;
+    window.enabled_buttons = EnabledButtons {
+        minimize: false,
+        maximize: false,
+        close: false,
+    };
+    window.decorations = false;
+    window.transparent = false;
+    window.focused = true;
+    window.visible = true;
 }
